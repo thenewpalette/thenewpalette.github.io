647658a94b740ffb3d3611b729801a92f14f6f82
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-05 21:38:52 -0500

    starting blog

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,16 @@
+// for some reason the used strict is not working?
+// $(function(){
+//    'use strict';
+
+//tabs
+$('#about-me').on('click', function (){
+  window.location = "aboutme.html";
+});
+
+$('#home').on('click', function (){
+  window.location = "index.html";
+});
+
+$('#food').on('click', function (){
+  window.location = "food.html";
+});
